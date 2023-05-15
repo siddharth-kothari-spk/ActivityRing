cad3aaabf440646dab43f5f6d9854f1ec7760263
sidkotha <sidkotha@cisco.com>
2023-05-15 22:35:52 +0530

    Initial Commit

---
+++ ActivityRing/ActivityRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingApp.swift
+//  ActivityRing
+//
+//  Created by sidkotha on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
