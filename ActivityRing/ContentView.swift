c3d162ff4b02470b29c9a0a2441b1b2b30243d71
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-05-15 22:50:27 +0530

    add an arc path

--- ActivityRing/ContentView.swift
+++ ActivityRing/ContentView.swift
@@ -27,8 +27,15 @@ struct ContentView_Previews: PreviewProvider {
 
 
 struct RingPath: Shape {
+    var percent: Double
+    
     func path(in rect: CGRect) -> Path {
         Path { path in
+            path.addArc(center: CGPoint(x: rect.width / 2, y: rect.height / 2), // center will be middle of the rect
+                        radius: rect.width / 2, // radius will be half of rect’s width
+                        startAngle: Angle(degrees: -90), // startAngle will be -90 degrees so our ring can start from top
+                        endAngle: Angle(degrees: ((percent / 100 * 360) + -90)), // endAngle will be computed from percent, and the startAngle value
+                        clockwise: false)
             
         }
     }
